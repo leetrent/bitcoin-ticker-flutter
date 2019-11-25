4e427b016c3fb8640a114f47b0d91425435e086f
LeeTrent <lee.trent.1@gmail.com>
2019-11-25 09:53:03 -0600

    Formats currency value

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter_money_formatter/flutter_money_formatter.dart';
 import 'package:http/http.dart' as http;
 
 const List<String> currenciesList = [
@@ -41,13 +42,20 @@ class CoinData {
     print('[CoinData][getCoinData] => (url): $url');
 
     http.Response response = await http.get(url);
-    print(
-        '[CoinData][getCoinData] => (response.statusCode): $response.statusCode');
+    print('[CoinData][getCoinData] => (response.statusCode): ' +
+        response.statusCode.toString());
 
     if (response.statusCode == 200) {
       var decodedData = jsonDecode(response.body);
       double lastPrice = decodedData['last'];
-      return lastPrice.toStringAsFixed(0);
+
+      print('[CoinData][getCoinData] => (lastPrice): ' + lastPrice.toString());
+
+      FlutterMoneyFormatter fmf = FlutterMoneyFormatter(amount: lastPrice);
+
+      //return lastPrice.toStringAsFixed(0);
+      //return fmf.output.nonSymbol;
+      return fmf.output.withoutFractionDigits;
     } else {
       throw 'Error: $response.statusCode';
     }
